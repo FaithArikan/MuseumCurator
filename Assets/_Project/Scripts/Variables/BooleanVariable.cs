22ac9672e3d8f370f8c99c01af285a0bb36dfc48
FaithArikan <fatiharikansmurf@gmail.com>
2022-09-08 17:07:39 +0300

    feat:Add BooleanVariable

---
+++ Assets/_Project/Scripts/Variables/BooleanVariable.cs
@@ -0,0 +1,22 @@
+﻿using ArcadeIdle.Helpers.Events;
+using UnityEngine;
+
+namespace ArcadeIdle.Variables
+{
+    [CreateAssetMenu(menuName = "Variables/Boolean")]
+    public class BooleanVariable : ScriptableObject
+    {
+        [SerializeField] private bool boolValue;
+        [SerializeField] private GameEvent onValueChanged;
+
+        public bool Value
+        {
+            get => boolValue;
+            set
+            {
+                boolValue = value;
+                if(onValueChanged != null) onValueChanged.Invoke();
+            }
+        }
+    }
+}
