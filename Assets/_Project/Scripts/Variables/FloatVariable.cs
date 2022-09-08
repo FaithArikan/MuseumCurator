708640ab981c7fc5683a35c77879313a0ce94080
FaithArikan <fatiharikansmurf@gmail.com>
2022-09-08 17:07:50 +0300

    feat: Add FloatVariable

---
+++ Assets/_Project/Scripts/Variables/FloatVariable.cs
@@ -0,0 +1,22 @@
+﻿using ArcadeIdle.Helpers.Events;
+using UnityEngine;
+
+namespace ArcadeIdle.Variables
+{
+    [CreateAssetMenu(menuName = "Variables/Float")]
+    public class FloatVariable : ScriptableObject
+    {
+        [SerializeField] private float floatValue;
+        [SerializeField] private GameEvent onValueChanged;
+
+        public float Value
+        {
+            get => floatValue;
+            set
+            {
+                floatValue = value;
+                if (onValueChanged != null) onValueChanged.Invoke();
+            }
+        }
+    }
+}
