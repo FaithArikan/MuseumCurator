43ea6cb91c84b3e7930c81495890e6d6d321a474
FaithArikan <fatiharikansmurf@gmail.com>
2022-09-08 17:08:01 +0300

    feat: Add IntVariable

---
+++ Assets/_Project/Scripts/Variables/IntVariable.cs
@@ -0,0 +1,22 @@
+﻿using ArcadeIdle.Helpers.Events;
+using UnityEngine;
+
+namespace ArcadeIdle.Variables
+{
+    [CreateAssetMenu(menuName = "Variables/Int")]
+    public class IntVariable : ScriptableObject
+    {
+        [SerializeField] private int intValue;
+        [SerializeField] private GameEvent onValueChanged;
+
+        public int Value
+        {
+            get => intValue;
+            set
+            {
+                intValue = value;
+                if (onValueChanged != null) onValueChanged.Invoke();
+            }
+        }
+    }
+}
