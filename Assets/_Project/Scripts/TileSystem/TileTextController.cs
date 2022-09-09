06b9781f06610e69f8af4a5c84e816fdc2df757a
FaithArikan <fatiharikansmurf@gmail.com>
2022-09-09 22:12:26 +0300

    feat: Add TileTextController

---
+++ Assets/_Project/Scripts/TileSystem/TileTextController.cs
@@ -0,0 +1,31 @@
+﻿using ArcadeIdle.Helpers;
+using UnityEngine;
+using TMPro;
+
+namespace ArcadeIdle.TileSystem
+{
+    public class TileTextController : MonoBehaviour
+    {
+        private Tile _tile;
+        [SerializeField] private TextMeshProUGUI priceText;
+
+        private void Awake()
+        {
+            _tile = GetComponent<Tile>();
+        }
+
+        private void OnEnable()
+        {
+            _tile.OnTileOpenAction += TileOnTileOpenAction; 
+        }
+        private void OnDisable()
+        {
+            _tile.OnTileOpenAction -= TileOnTileOpenAction; 
+        }
+
+        private void TileOnTileOpenAction(int value)
+        {
+            priceText.text = Utils.FormatCash(value);
+        }
+    }
+}
