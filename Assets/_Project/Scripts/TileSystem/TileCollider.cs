a4bbfce87fda31ca30240f2b4b7096417c52ba52
FaithArikan <fatiharikansmurf@gmail.com>
2022-09-09 22:11:29 +0300

    feat: Add TileCollider

---
+++ Assets/_Project/Scripts/TileSystem/TileCollider.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+namespace ArcadeIdle.TileSystem
+{
+    public class TileCollider : MonoBehaviour
+    {
+        [SerializeField] private BoxCollider outCol;
+        [SerializeField] private BoxCollider inCol;
+        
+        private Tile _tile;
+
+        private void Awake()
+        {
+            _tile = GetComponent<Tile>();
+        }
+
+        private void OnEnable()
+        {
+            _tile.OnColliderAction += OnThisTileOpened;
+        }
+
+        private void OnThisTileOpened(bool b)
+        {
+            outCol.isTrigger = b;
+            outCol.enabled = b;
+            
+            inCol.isTrigger = !b;
+            inCol.enabled = b;
+        }
+
+        private void OnDisable()
+        {
+            _tile.OnColliderAction -= OnThisTileOpened;
+        }
+    }
+}
