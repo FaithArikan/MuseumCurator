96e5cbfc3cf21e5e97ef60437c68592944e57bf3
FaithArikan <fatiharikansmurf@gmail.com>
2022-09-15 19:27:01 +0300

    feat: Add GameLoadManager

---
+++ Assets/_Project/Scripts/Core/GameLoadManager.cs
@@ -0,0 +1,28 @@
+﻿using ArcadeIdle.Helpers;
+using ArcadeIdle.Helpers.Events;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace ArcadeIdle.Core
+{
+    public class GameLoadManager : Singleton<GameLoadManager>
+    {
+        [SerializeField] private SceneReferance sceneReferance;
+        [SerializeField] private GameEvent onGameStarted;
+        private void Start()
+        {
+            Application.targetFrameRate = 60;
+            LoadSceneAdditive();
+        }
+
+        private void LoadSceneAdditive()
+        {
+            for (int i = 0; i < sceneReferance.beginScenes.Count; i++)
+            {
+                SceneManager.LoadScene((string) sceneReferance.beginScenes[i].name, LoadSceneMode.Additive);
+            }
+            
+            onGameStarted.Invoke();
+        }
+    }
+}
