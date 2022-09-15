bb2120e11627ee79a81da9a329428daa95cf9504
FaithArikan <fatiharikansmurf@gmail.com>
2022-09-15 19:27:12 +0300

    feat: Add SceneReferance

---
+++ Assets/_Project/Scripts/Core/SceneReferance.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+
+namespace ArcadeIdle.Core
+{
+    [Serializable]
+    public class SceneReferance
+    {
+        public List<SceneAsset> beginScenes;
+    }
+    
+}
