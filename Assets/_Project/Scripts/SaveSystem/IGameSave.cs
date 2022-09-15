6539a95155775fba4a97d96ac3d14a58de61c839
FaithArikan <fatiharikansmurf@gmail.com>
2022-09-15 19:23:55 +0300

    feat: Add IGameSave

---
+++ Assets/_Project/Scripts/SaveSystem/IGameSave.cs
@@ -0,0 +1,12 @@
+﻿namespace ArcadeIdle.SaveSystem
+{
+    public interface IGameSave
+    {
+        void Save(string filename, object data);
+        T Load<T>(string filename) where T : new();
+        void Delete(string filename);
+        bool FileExists(string filename);
+        string GetFullPath(string filename);
+        public void WriteToDisk();
+    }
+}
