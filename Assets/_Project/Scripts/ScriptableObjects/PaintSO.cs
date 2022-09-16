8765043cf2c2fcd7b3aac98b002dc3f704b27eb5
FaithArikan <fatiharikansmurf@gmail.com>
2022-09-16 19:04:54 +0300

    feat: Add PaintSo

---
+++ Assets/_Project/Scripts/ScriptableObjects/PaintSO.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using ArcadeIdle.Helpers.Events;
+using ArcadeIdle.SaveSystem;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace ArcadeIdle.ScriptableObjects
+{
+    [CreateAssetMenu(menuName = "Paint", order = 0)]
+    public class PaintSO : ScriptableObject
+    {
+        [SerializeField] private PaintType paintType;
+        [SerializeField] private Material material;
+        
+        [SerializeField] private int amount;
+        [SerializeField] private GameEvent onResourceValueChanged;
+
+        public UnityAction<Material> OnPaintSelected = delegate { };
+        
+        public int Amount
+        {
+            get => amount;
+            set
+            {
+                if (amount == value) return;
+                onResourceValueChanged.Invoke();
+            }
+        }
+
+        public void SelectPaint()
+        {
+            Debug.Log(Amount);
+            OnPaintSelected?.Invoke(material);
+            amount--;
+            Debug.Log(Amount);
+        }
+    }
+}
