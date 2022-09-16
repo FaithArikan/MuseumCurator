d37ef53a739c7c86dc63a3864b3e8ebf59c608ec
FaithArikan <fatiharikansmurf@gmail.com>
2022-09-16 19:05:08 +0300

    feat: Add Resource

---
+++ Assets/_Project/Scripts/ResourceSystem/Resource.cs
@@ -0,0 +1,18 @@
+﻿using ArcadeIdle.ScriptableObjects;
+using UnityEngine;
+
+namespace ArcadeIdle.ResourceSystem
+{
+    public class Resource : MonoBehaviour
+    {
+        [SerializeField] private ResourceSO resourceSo;
+        public ResourceSO ResourceSo => resourceSo;
+        [SerializeField] private int sumAmount;
+
+        public int SumAmount
+        {
+            get => sumAmount;
+            set => sumAmount = value;
+        }
+    }
+}
