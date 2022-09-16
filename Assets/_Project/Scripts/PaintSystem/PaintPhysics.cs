862b488d3bb58a9ebf3a8701144d60226e801d29
FaithArikan <fatiharikansmurf@gmail.com>
2022-09-16 19:06:09 +0300

    feat: Add PaintPhysics

---
+++ Assets/_Project/Scripts/PaintSystem/PaintPhysics.cs
@@ -0,0 +1,27 @@
+﻿using ArcadeIdle.Helpers.Events;
+using UnityEngine;
+
+namespace ArcadeIdle.PaintSystem
+{
+    public class PaintPhysics : MonoBehaviour
+    {
+        [SerializeField] private GameEvent onPaintButtonShow;
+        [SerializeField] private GameEvent onPaintButtonHide;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                onPaintButtonShow.Invoke();
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                onPaintButtonHide.Invoke();
+            }
+        }
+    }
+}
