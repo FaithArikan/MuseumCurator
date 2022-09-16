229bbdff5aba4971e495e886911b0b4d4da3db13
FaithArikan <fatiharikansmurf@gmail.com>
2022-09-16 19:10:05 +0300

    feat: Add ResourcePicker

---
+++ Assets/_Project/Scripts/Player/ResourcePicker.cs
@@ -0,0 +1,21 @@
+﻿using ArcadeIdle.ResourceSystem;
+using UnityEngine;
+
+namespace ArcadeIdle.Player
+{
+    public class ResourcePicker : MonoBehaviour
+    {
+        [SerializeField] private AudioSource resourcePickAudioSource;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("Resource"))
+            {
+                Resource res = other.gameObject.GetComponent<Resource>();
+                res.ResourceSo.Amount += res.SumAmount;
+                resourcePickAudioSource.Play();
+                Destroy(other.gameObject);
+            }
+        }
+    }
+}
